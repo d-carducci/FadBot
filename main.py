fdabd5ddba18308170b74c8dbbd3d8dfadef3f6e
D.Carducci <carduccidavide327@gmail.com>
2021-07-20 12:01:58 +0200

    Minimum viable version! added alt-text for images, error handling in update(), and a simple console prompt to run the script. We'll see in about an hour if this actually works.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tweepy
 from json import load
-
+import sys
 
 dump_text = 1
 
@@ -12,12 +12,14 @@ with open('text.json', 'r') as text_file:
     animal_dict = {key: ''.join(lines) for key, lines in text['animals'].items()}
     buyer_dict = {key: ''.join(lines) for key, lines in text['buyers'].items()}
     extra_dict = {key: ''.join(lines) for key, lines in text['extras'].items()}
+    metadata_dict = {key: ''.join(lines) for key, lines in text['metadata'].items()}
 
     master_dict = {'intro_' + key: tweet for key, tweet in intro_dict.items()}
     master_dict.update({'quality_' + key: tweet for key, tweet in quality_dict.items()})
     master_dict.update({'animal_' + key: tweet for key, tweet in animal_dict.items()})
     master_dict.update({'buyer_' + key: tweet for key, tweet in buyer_dict.items()})
     master_dict.update({'extra_' + key: tweet for key, tweet in extra_dict.items()})
+    master_dict.update({'metadata_' + key: tweet for key, tweet in metadata_dict.items()})
 
     if dump_text:
         with open('text_dump.txt', 'w') as dump_file:
@@ -32,8 +34,8 @@ def create_intro(*args):
         intro += intro_dict[arg]
     return intro
 
-def login():
 
+def login():
     with open('twtcred.json', 'r') as cred_file:
         keys = load(cred_file)
         consumer_key = keys['KEY']
@@ -51,18 +53,44 @@ def login():
         print("Error during authentication")
     return api
 
+
 def update(quality, animal, buyer):
     intro = create_intro(quality, animal, buyer)
     api = login()
-    tweets = [intro, quality_dict[quality], animal_dict[animal], buyer_dict[buyer]]
-    reply_id = None
-    for tweet in tweets:
-        status = api.update_status(status=tweet,
-                                 in_reply_to_status_id=reply_id,
-                                 auto_populate_reply_metadata=True)
-        reply_id = status.id
+    tweets = [intro, quality_dict[quality], extra_dict[quality], animal_dict[animal], buyer_dict[buyer]]
+    # tweets = ['test 1', 'test 2', 'test 3', 'test 4']
+    structure = ["intro", "quality", "quality_extra", "animal", "buyer"]
+    if buyer in extra_dict.keys():
+        tweets.append(extra_dict[buyer])
+        structure.append("buyer_extra")
 
-login()
+    reply_id = None
+    img_id = None
+    for tweet, kind in zip(tweets, structure):
+        if kind == 'quality':
+            img = api.media_upload("images/{}.png".format(quality))
+            api.create_media_metadata(img.media_id, metadata_dict[quality])
+            img_id = [img.media_id]
+
+        try:
+            status = api.update_status(status=tweet, media_ids=img_id, in_reply_to_status_id=reply_id,
+                                       auto_populate_reply_metadata=True)
+            if status:
+                print('posted {} tweet'.format(kind))
+                reply_id = status.id
+        except tweepy.error.TweepError as e:
+            print(e)
+            sys.exit('Status update failed')
+
+        img_id = None
+
+    print('Status update successful')
+
+
+update_input = input("Enter the Quality/Animal/Buyer, separated by spaces: ")
+fad_data = update_input.split(' ')
+update(*fad_data)
+
 '''
 
 api = tweepy.API(auth)
